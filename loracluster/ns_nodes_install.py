fffe8bcc7cc1729125230c26ee8393a8adee7b71
Ben <benjaminpablotran@gmail.com>
2025-07-22 12:30:04 -0400

    Add function to loop through nodes and printresults

--- loracluster/ns_nodes_install.py
+++ loracluster/ns_nodes_install.py
@@ -26,12 +26,40 @@ def connect_ssh(host, username, password) -> bool:
         print("Authentication failed: Wrong username or password.")
         return False
 
+def connect_all_nodes() -> list:
+    results = []
+    for section in config.sections():
+        if section != 'settings':
+            gw_connection_status = [section, connect_ssh(config[section]["host"], config[section]["username"], config[section]["password"])]
+            results.append(gw_connection_status)
+    return results
+
+def print_connect_results(results) -> None:
+    success = 0
+    failed = 0
+    for result in results:
+        if result[1]:
+            print(result[0] + " successfully connected")
+            success+=1
+        else:
+            print(result[0] + " failed to connect")
+            failed+=1
+    print(f"{success}/{success + failed} successfully connected")
+
+    
+ 
 def main() -> int:
     """
     Main Function.
     """
-    print("Calling the function")
+    print("Testing connect SSH")
     connect_ssh('102.199.1.20', 'ben', 'password123')
+    print("Testing looping nodes")
+    print_connect_results(connect_all_nodes())
+    continue_status = input("Would you like to continue? (Y/N)")
+    if continue_status == "N":
+        return 0
+    print("Running future scripts...")
     return 0
 
 if __name__ == "__main__":
